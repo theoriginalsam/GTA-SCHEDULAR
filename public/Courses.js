48a3bdd09acb7bb362f3e0c2dbcad1d20e9169a3
Samantha Solomon <31627377+Pocahontas09@users.noreply.github.com>
2024-03-22 00:51:28 -0500

    Add files via upload

---
+++ public/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Courses.css';
+
+function Courses() {
+  return (
+    <div className="courses">
+      {/* Courses list and add course button */}
+    </div>
+  );
+}
+
+export default Courses;
