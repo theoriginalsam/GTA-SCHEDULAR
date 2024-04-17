1dae0170e66c65912083141654ec0a8ae4b4ae28
Samantha Solomon <31627377+Pocahontas09@users.noreply.github.com>
2024-04-17 17:26:04 -0500

    Update User.js
    removed roles

--- backend/User.js
+++ backend/User.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true }, // Assuming you're using email as the username
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true }
+ 
 });
 
 
